6143bbcaa508a80df7b769b88c704ee84fe6da62
martincz <martincz@mokeetek.com>
2013-05-26 23:50:36 +0800

    kernel path
    Change-Id: Ied4368cba2dbcf03b9acf4578ae8fb3e6cca528a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -166,7 +166,7 @@ hboot:
 	make clean -C $(ANDROID_BUILD_TOP)/device/motorola/jordan-common/hboot
 
 # If kernel sources are present in repo, here is the location
-TARGET_KERNEL_SOURCE := $(ANDROID_BUILD_TOP)/jordan-kernel
+TARGET_KERNEL_SOURCE := kernel/motorola/jordan
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
 BOARD_KERNEL_CMDLINE := console=/dev/null mem=498M init=/init omapfb.vram=0:4M
 #TARGET_PREBUILT_KERNEL := $(ANDROID_BUILD_TOP)/device/motorola/jordan-common/kernel
