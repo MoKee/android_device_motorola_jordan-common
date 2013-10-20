b6d96b6a6c903c23c21a9f18b9afe4069e570ab1
martincz <martincz@mokeetek.com>
2013-10-20 14:00:59 +0800

    mokee modify
    Change-Id: I8b1fff01b3da9b3608d28a681ef7004c7de00dd3

--- cmhw/org/mokee/hardware/LongTermOrbits.java
+++ cmhw/org/mokee/hardware/LongTermOrbits.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 The CyanogenMod Project
+ * Copyright (C) 2013 The MoKee OpenSource Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.cyanogenmod.hardware;
+package org.mokee.hardware;
 
 import java.io.File;
 import android.util.Log;
